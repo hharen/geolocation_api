29d04183d677c1a209980da0232d220018174a80
Hana Harencarova <h.harencarova@gmail.com>
2022-01-30 17:03:43 +0100

    Add create action

--- app/controllers/geolocation_objects_controller.rb
+++ app/controllers/geolocation_objects_controller.rb
@@ -1,11 +1,45 @@
 class GeolocationObjectsController < ApplicationController
+  require 'uri'
+  require 'net/http'
+
+  SERVICE_PROVIDER = 'http://api.ipstack.com/'
+  API_KEY = ENV['ip_stack_api_key']
+
   def index
-    geolocation_object = GeolocationObject.where(ip: params['query']).first
+    geolocation_object = GeolocationObject.find_by(ip: params['query'])
 
     if geolocation_object.present?
       respond_json(status: :ok, response: geolocation_object)
     else 
-      respond_json(status: :not_found) #TO DO: test this
+      respond_json(status: :not_found)
+    end
+  end
+
+  def create
+    ip = params[:query]
+
+    uri = URI("#{SERVICE_PROVIDER}/#{ip}?access_key=#{API_KEY}")
+    response = Net::HTTP.get_response(uri)
+    body = JSON.parse(response.body).with_indifferent_access
+
+    geolocation_object = GeolocationObject.new(
+      url: body[:url],
+      ip: body[:ip],
+      ip_type: body[:type],
+      continent_code: body[:continent_code],
+      continent_name: body[:continent_name],
+      country_code: body[:country_code],
+      country_name: body[:country_name],
+      region_code: body[:region_code],
+      region_name: body[:region_name],
+      city: body[:city],
+      zip: body[:zip]
+    )
+
+    if geolocation_object.save
+      respond_json(status: :created)
+    else
+      respond_json(status: :unprocessable_entity)
     end
   end
 
@@ -19,9 +53,3 @@ class GeolocationObjectsController < ApplicationController
     end
   end
 end
-
-# TO DO:
-# Errors:
-# query can't be null
-
-#Add? Accept: application/vnd.api+json
