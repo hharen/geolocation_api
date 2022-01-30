14a72a6b829be9e1f1089dfc2af5fb5025379a8c
Hana Harencarova <h.harencarova@gmail.com>
2022-01-30 16:19:39 +0100

    Add index controller action with spec

--- app/controllers/geolocation_objects_controller.rb
+++ app/controllers/geolocation_objects_controller.rb
@@ -1,2 +1,27 @@
 class GeolocationObjectsController < ApplicationController
+  def index
+    geolocation_object = GeolocationObject.where(ip: params['query']).first
+
+    if geolocation_object.present?
+      respond_json(status: :ok, response: geolocation_object)
+    else 
+      respond_json(status: :not_found) #TO DO: test this
+    end
+  end
+
+  private 
+
+  def respond_json(status: :ok, response: nil, content_type: {"Content-Type" => "application/vnd.api+json"})
+    if response.present?
+      render status: status, headers: content_type, json: { data: response }
+    else
+      head status, content_type
+    end
+  end
 end
+
+# TO DO:
+# Errors:
+# query can't be null
+
+#Add? Accept: application/vnd.api+json
---
+++ spec/requests/geolocation_object_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'GeolocationObject', :type => :request do
+  fixtures :all
+
+  let(:headers) { { "ACCEPT" => "application/vnd.api+json" } }
+
+  context 'with valid query' do
+    it 'returns correct geolocation object' do
+      get '/geolocation_objects/?query=84.254.92.149', :headers => headers
+      body = JSON.parse(response.body).with_indifferent_access
+
+      expect(response.content_type).to eq("application/json; charset=utf-8")
+      expect(response).to have_http_status(:ok)
+      expect(body).to have_key('data')
+      expect(body.dig(:data, :ip)).to eq('84.254.92.149')
+    end
+  end
+
+  context 'with invalid query' do
+    it 'returns 404' do
+      get '/geolocation_objects/?query=not-a-ip-address', :headers => headers
+
+      expect(response.content_type).to eq("application/vnd.api+json")
+      expect(response).to have_http_status(:not_found)
+      expect(response.body).to eq('')
+
+    end
+  end
+end
